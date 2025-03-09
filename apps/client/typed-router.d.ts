3a22f625c0d1e4045bea871bb6e08fe1208862c5
fomnn <95164774+fomnn@users.noreply.github.com>
2025-03-09 16:36:30 +0700

    update

--- apps/client/typed-router.d.ts
+++ apps/client/typed-router.d.ts
@@ -20,6 +20,5 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
-    '/auth/login': RouteRecordInfo<'/auth/login', '/auth/login', Record<never, never>, Record<never, never>>,
   }
 }
