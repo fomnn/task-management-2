cb999b69d5127916c77e1235164420cc1ea66fb7
fomnn <95164774+fomnn@users.noreply.github.com>
2025-03-09 17:07:27 +0700

    update

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,21 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import authApi from "./routes/auth.js";
+import { prisma } from "./lib/prisma.js";
 
 const app = new Hono();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
+
+app.get("/users", async (c) => {
+  const users = await prisma.users.findMany();
+
+  return c.json({
+    users
+  });
+})
 
 app.route("/", authApi);
 
